7d5931f8d0858b5a92ad4f2739b1d1bd17476b20
Lucas Pimenta <lucas.duane@gmail.com>
2020-11-12 21:18:17 -0400

    Initial commit

---
+++ lib/reviews_scraper.ex
@@ -0,0 +1,18 @@
+defmodule ReviewsScraper do
+  @moduledoc """
+  Documentation for `ReviewsScraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReviewsScraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/reviews_scraper_test.exs
@@ -0,0 +1,8 @@
+defmodule ReviewsScraperTest do
+  use ExUnit.Case
+  doctest ReviewsScraper
+
+  test "greets the world" do
+    assert ReviewsScraper.hello() == :world
+  end
+end
